5344faeea5ee8e84f564700bc216396ef4b1cb35
Ariel <ariel.wm1@gmail.com>
2020-06-05 13:32:51 -0400

    Create Task
    Add DTO

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common'
-import { Task } from './task.model'
+import { Task, TaskStatus } from './task.model'
+import { CreateTaskDto } from './dto/create-task.dto'
+// import * as uuid from 'uuid' UNINSTALLLLLLLL
 
 @Injectable()
 export class TasksService {
@@ -7,5 +9,18 @@ export class TasksService {
 
   getAllTasks(): Task[] {
     return this.tasks
+  }
+
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto
+    const task: Task = {
+      id: '12312312',
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    }
+
+    this.tasks.push(task)
+    return task
   }
 }
